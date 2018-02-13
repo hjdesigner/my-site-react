98e6d0fd6362d1d3a72caccc01a006670aa536a2
Henrique Melanda <hjdesigner23@gmail.com>
2018-02-13 21:29:04 -0200

    Base of project

---
+++ src/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import React, { Component } from 'react'
+import ajax from '@fdaciuk/ajax'
+import AppContent from 'components/app-content'
+
+class App extends Component {
+  render () {
+    return <AppContent />
+  }
+
+}
+
+export default App
+
---
+++ src/components/app-content/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React, { PropTypes } from 'react'
+
+const AppContent = () => (
+  <div>
+    <h1>Teste</h1>
+  </div>
+)
+
+export default AppContent
